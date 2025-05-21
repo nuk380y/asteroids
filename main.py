38a3ffb00712955dc476584bd97a5bc3dca6b9ac
David Dean <runfromnuke@gmail.com>
2025-05-21 16:36:48 -0500

    feat: initialize game clock
    Attempt to framelock the game at 60 FPS.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +19,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
 
 
 # Initialize pygame
