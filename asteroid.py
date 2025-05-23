3eb4167fc7209150ac66e55e1c97e64feec76d32
nuk380y <runfromnuke@gmail.com>
2025-05-23 17:06:31 -0500

    feat: shots split asteroids
    Collisions between shots now split large and medium asteroids into two
    asteroids of the next smaller size.  Small asteroids are removed and do
    not split.

--- asteroid.py
+++ asteroid.py
@@ -7,8 +7,9 @@ from constants import *
 
 
 class Asteroid(CircleShape):
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius, velocity=None):
         super().__init__(x, y, radius)
+        self.velocity = velocity
 
     def draw(self, screen):
         # pygame.draw.circle(screen, "white", (self.x, self.y), self.radius, 2)
@@ -16,3 +17,34 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def rotate(self, divert):
+        bisected = divert / 2
+        change_direction = pygame.Vector2(
+                self.velocity.x + bisected * 1.2,
+                self.velocity.y + bisected * 1.2)
+        return change_direction
+
+    def split(self):
+        self.kill()
+        # Determine if if this shot asteroid needs to split or not.
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            # Calculate new paths for slit asteroids
+            divert = random.uniform(20, 50)
+            new_vector1 = self.rotate(divert)
+            new_vector2 = self.rotate(-divert)
+            # Calculate size of new asteroids
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            # Spawn new asteroids at collision point
+            asteroid1 = Asteroid(
+                    self.position.x,
+                    self.position.y,
+                    new_radius,
+                    new_vector1)
+            asteroid2 = Asteroid(
+                    self.position.x,
+                    self.position.y,
+                    new_radius,
+                    new_vector2)
--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collision(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         dt = game_clock.tick(60) / 1000
